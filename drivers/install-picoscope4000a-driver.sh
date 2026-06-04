8948e6aa8e9be28add88362addd55799d7aa97dc
Henry Gotjen <hgg@mercury>
2026-06-04 15:49:54 -0700

    permissions fix

--- drivers/install-picoscope4000a-driver.sh
+++ drivers/install-picoscope4000a-driver.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-wget -O- https://labs.picotech.com/Release.gpg.key | sudo gpg --dearmor > /usr/share/keyrings/picotech-archive-keyring.gpg
+wget -O- https://labs.picotech.com/Release.gpg.key | sudo gpg --dearmor | sudo tee /usr/share/keyrings/picotech-archive-keyring.gpg > /dev/null
 echo "deb [signed-by=/usr/share/keyrings/picotech-archive-keyring.gpg] https://labs.picotech.com/picoscope7/debian/ picoscope main" | sudo tee /etc/apt/sources.list.d/picoscope7.list
 sudo apt-get update && sudo apt-get install libps4000a
 
